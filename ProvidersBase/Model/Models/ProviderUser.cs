9191b941803e467ab7c26ab90799e6606e511919
44East <antswert@gmail.com>
2023-06-10 17:56:51 +0300

    Create model and context

---
+++ ProvidersBase/Model/Models/ProviderUser.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProvidersBase.Model.Models
+{
+    [Table("Users")]
+    public class ProviderUser
+    {
+        [Key, Column("user_id")]
+        public int Id { get; set; }
+
+        [Column("provider_id")]
+        public int ProviderId { get; set; }
+
+        [ForeignKey(nameof(ProviderId))]
+        public ProviderCompany Provider { get; set; }
+
+        [Column("name"), Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column("username"),  Required, MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Column("email"), Required, EmailAddress, MaxLength(254)]
+        public string Email { get; set; }
+
+        [Column("phone_number"),  Required, Phone, MaxLength(10)]
+        public string PhoneNumber { get; set; }
+    }
+}
